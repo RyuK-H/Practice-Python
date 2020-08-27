b0ee843f659dbf8fd951aa938be3d68f5bd5f673
RyuK-H <ryugihyeok@gmail.com>
2020-08-27 23:54:07 +0900

    random 함수

--- practice.py
+++ practice.py
@@ -29,3 +29,14 @@ from math import *
 print(floor(4.99))
 print(ceil(3.14))
 print(sqrt(15))
+
+from random import *
+
+print(random())
+print(int(random() * 10))
+print(int(random() * 45) + 1)
+print(int(random() * 45) + 1)
+print(randrange(1,46))
+print(randrange(1,46))
+print(randrange(1,46))
+print(randint(1,45))
