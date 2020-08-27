a9be3b5f4978d4b7093ae884eec3229c0f3b509c
RyuK-H <ryugihyeok@gmail.com>
2020-08-27 23:45:12 +0900

    문자형이 아니면 str로 감싸서 형변환 해야하는듯

--- practice.py
+++ practice.py
@@ -1,2 +1,12 @@
 print(True)
 print(not True)
+
+animal = "인간"
+name = "류기혁"
+age = 27
+hobby = "망상"
+is_adult = age > 20
+
+print("우리집" + animal + "의 이름은 " + name + "에여")
+print("정수형은 str로 감싸야한데 ㄷㄷ" + str(age))
+print("bool은?" + str(is_adult))
