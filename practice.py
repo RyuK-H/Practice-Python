2bdebb061d4844ec41af4dcc3e6a2f6b60ac4eb8
RyuK-H <ryugihyeok@gmail.com>
2020-09-20 00:06:52 +0900

    클래스

--- practice.py
+++ practice.py
@@ -244,5 +244,67 @@
 # with open("study.txt", "w", encoding="utf8") as study_file:
 #   study_file.write("파이썬 공부 중")
 
-with open("study.txt", "r", encoding="utf8") as study_file:
-  print(study_file.read())
+# with open("study.txt", "r", encoding="utf8") as study_file:
+#   print(study_file.read())
+
+# name = "마린"
+# hp = 40
+# damage = 5
+
+# print("{} 유닛이 생성 되었습니다.".format(name))
+# print("체력 {0}, 공격력 {1}\n".format(hp, damage))
+
+# tank_name = "탱크"
+# tank_hp = 150
+# tank_damage = 35
+
+# print("{} 유닛이 생성 되었습니다.".format(tank_name))
+# print("체력 {0}, 공격력 {1}\n".format(tank_hp, tank_damage))
+
+# def attack(name, location, damage):
+#   print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(name, location, damage))
+
+# attack(name, "1시", damage)
+# attack(tank_name, "1시", tank_damage)
+
+class Unit:
+  def __init__(self, name, hp):
+    self.name = name
+    self.hp = hp
+
+class AttackUnit(Unit):
+  def __init__(self, name, hp, damage):
+    Unit.__init__(self, name, hp)
+    self.damage = damage
+
+  def attack(self, location):
+    print("{0} : {1} 방향으로 적군을 공격 합니다. [공격력 {2}]".format(self.name, location, self.damage))
+
+  def damaged(self, damage):
+    print("{0} : {1} 데미지를 입었습니다.".format(self.name, self.damage))
+    self.hp -= damage
+    print("{0} : 현재 체력은 {1} 입니다.".format(self.name, self.hp))
+    if self.hp <= 0:
+      print("{0} : 파괴되었습니다.".format(self.name))
+
+class Flyable:
+  def __init__(self, flying_speed):
+    self.flying_speed = flying_speed
+
+  def fly(self, name, location):
+    print("{0} : {1} 방향으로 날아갑니다. [속도 {2}].".format(name, location, self.flying_speed))
+
+class FlyableAttackUnit(AttackUnit, Flyable):
+  def __init__(self, name, hp, damage, flying_speed):
+    AttackUnit.__init__(self, name, hp, damage)
+    Flyable.__init__ (self, flying_speed)
+
+
+valkyrie = FlyableAttackUnit("발키리", 200, 6, 5)
+valkyrie.fly(valkyrie.name, "3시")
+
+firebat1 = AttackUnit("파이어뱃", 50, 16)
+firebat1.attack("5시")
+
+firebat1.damaged(25)
+firebat1.damaged(25)
